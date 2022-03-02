ee5a1bc78b0acee2396c52e14d88ae4df605a346
Aleksandr F <a7214370@gmail.com>
2022-03-02 14:27:24 +0300

    Setup task completed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var greetingRedLight: UIView!
+    @IBOutlet var greetingOrangeLight: UIView!
+    @IBOutlet var greetingGreenLight: UIView!
+    @IBOutlet var greetingStartButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingRedLight.layer.cornerRadius = 50
+        greetingRedLight.alpha = 0.3
+        
+        greetingOrangeLight.layer.cornerRadius = 50
+        greetingOrangeLight.alpha = 0.3
+        
+        greetingGreenLight.layer.cornerRadius = 50
+        greetingGreenLight.alpha = 0.3
+        
+        greetingStartButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func greetingStartButtonPressed() {
+        greetingStartButton.setTitle("NEXT", for: .normal)
+        
+        if greetingRedLight.alpha == 1 {
+            greetingRedLight.alpha = 0.3
+            greetingOrangeLight.alpha = 1
+        } else if greetingOrangeLight.alpha == 1 {
+            greetingOrangeLight.alpha = 0.3
+            greetingGreenLight.alpha = 1
+        } else {
+            greetingGreenLight.alpha = 0.3
+            greetingRedLight.alpha = 1
+        }
+    }
 }
+    
+
 
